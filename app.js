d2768736dc87053ae8055942a756d8e04764b313
Palak Chhallani <palakchhallani@gmail.com>
2026-02-22 13:44:36 +0530

    Upgrade the secret key and sessions

--- app.js
+++ app.js
@@ -51,11 +51,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SECRET,
-  },
 });
-
 store.on("error", (err) => {
   console.log("ERROR in MONGO SESSION STORE", err);
 });
