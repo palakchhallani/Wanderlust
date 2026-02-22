11014342ba6345f1d29dfb3c1efffa28ba37f493
Palak Chhallani <palakchhallani@gmail.com>
2026-02-22 10:03:41 +0530

    Fix double response error handler

--- app.js
+++ app.js
@@ -109,6 +109,10 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
   const statusCode = err.statusCode || 500;
   const message = err.message || "Something went wrong";
 
