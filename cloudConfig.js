f0cab4cf1b35cbdee19c549a5f75d6370b9b0831
Palak Chhallani <palakchhallani@gmail.com>
2026-02-22 14:14:58 +0530

    Fixed CloudinaryStorage import for v3 CommonJS

--- cloudConfig.js
+++ cloudConfig.js
@@ -7,9 +7,7 @@ cloudinary.config({
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
-console.log("CLOUD_NAME:", process.env.CLOUD_NAME);
-console.log("CLOUD_API_KEY:", process.env.CLOUD_API_KEY);
-console.log("CLOUD_API_SECRET:", process.env.CLOUD_API_SECRET);
+console.log("Cloudinary object:", typeof cloudinary);
 
 const storage = new CloudinaryStorage({
   cloudinary,
