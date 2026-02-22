a3a19824797cb7c434f931bb26664f96e62318f8
Palak Chhallani <palakchhallani@gmail.com>
2026-02-22 13:55:15 +0530

    changes in cloudconfig

--- cloudConfig.js
+++ cloudConfig.js
@@ -7,6 +7,8 @@ cloudinary.config({
   api_secret: process.env.CLOUD_API_SECRET,
 });
 console.log("CLOUD_NAME:", process.env.CLOUD_NAME);
+console.log("CLOUD_API_KEY:", process.env.CLOUD_API_KEY);
+console.log("CLOUD_API_SECRET:", process.env.CLOUD_API_SECRET);
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
